697b9d4445edd7f00571a8011427dd2a6a211240
Bennet Hölscher <bennet.hoelscher@outlook.com>
2018-12-02 14:09:24 +0100

    Added FindAppointmentsBySubject() and FindAppointmentsByLocation() to the class Calendar

--- Outlook-Access/Calendar.cs
+++ Outlook-Access/Calendar.cs
@@ -15,6 +15,8 @@ namespace Outlook_Access
         private Outlook.MAPIFolder _OutlookCalendar;
         private Outlook.Items _CalendarItems;
 
+        /* Constructors */
+
         public Calendar(string pUsername, string pPassword, bool pShowDialog, bool pNewSession, string pNamespace, string pFolderID)
         {
             try
@@ -58,6 +60,7 @@ namespace Outlook_Access
 
         public Calendar() : this(null) { }
 
+        /* Public methods */
 
         public void Disconnect()
         {
@@ -88,6 +91,104 @@ namespace Outlook_Access
             }
         }
 
+        //Returns every appointment with a given subject within a given interval
+        public Outlook.Items FindAppointmentsBySubject(string pSubject, DateTime pStart, DateTime pEnd)
+        {
+            return RestrictBySubject(FindAppointmentsInRange(pStart, pEnd), pSubject);
+        }
 
+        //Returns every appointment with one of the given subjects within a given interval
+        public Outlook.Items FindAppointmentsBySubject(string[] pSubjects, DateTime pStart, DateTime pEnd)
+        {
+            Outlook.Items apptsInInterval = FindAppointmentsInRange(pStart, pEnd);
+            Outlook.Items appts = new Outlook.Items();
+            foreach (string subject in pSubjects)
+            {
+                foreach (Outlook.AppointmentItem item in RestrictBySubject(apptsInInterval, subject))
+                {
+                    appts.Add(item);
+                }
+            }
+            appts.Sort("[Start]", Type.Missing);
+            return appts;
+        }
+
+        //Returns every appointment in a given location within a given interval 
+        public Outlook.Items FindAppointmentsByLocation(string pLocation, DateTime pStart, DateTime pEnd)
+        {
+            return RestrictByLocation(FindAppointmentsInRange(pStart, pEnd), pLocation);
+        }
+
+        //Returns every appointment with one of the given locations within a given interval
+        public Outlook.Items FindAppointmentsByLocation(string[] pLocations, DateTime pStart, DateTime pEnd)
+        {
+            Outlook.Items apptsInInterval = FindAppointmentsInRange(pStart, pEnd);
+            Outlook.Items appts = new Outlook.Items();
+            foreach (string location in pLocations)
+            {
+                foreach (Outlook.AppointmentItem item in RestrictByLocation(apptsInInterval, location))
+                {
+                    appts.Add(item);
+                }
+            }
+            appts.Sort("[Start]", Type.Missing);
+            return appts;
+        }
+
+        /* Methods for class internal use */
+
+        //Restrict a list of appointments to the ones with a given subject
+        private Outlook.Items RestrictBySubject(Outlook.Items pAppointments, string pSubject)
+        {
+            StringBuilder subjectFilter = new StringBuilder();
+            subjectFilter.Append("@SQL=" + "\"" + "urn:schemas:httpmail:subject" + "\"");
+
+            if (_OutlookApplication.Session.DefaultStore.IsInstantSearchEnabled)
+            {
+                subjectFilter.Append(@" ci_startswith '{pSubject}'");
+            }
+            else
+            {
+                subjectFilter.Append(@" like '%{pSubject}%'");
+            }
+
+            Outlook.Items appts = pAppointments;
+            appts.Restrict(subjectFilter.ToString());
+            if (appts.Count > 0)
+            {
+                return appts;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        //Restrict a list of appointments to the ones with  a given location
+        private Outlook.Items RestrictByLocation(Outlook.Items pAppointments, string pLocation)
+        {
+            StringBuilder locationFilter = new StringBuilder();
+            locationFilter.Append("@SQL=" + "\"" + "urn:schemas:httpmail:location" + "\"");
+
+            if (_OutlookApplication.Session.DefaultStore.IsInstantSearchEnabled)
+            {
+                locationFilter.Append(@" ci_startswith '{pLocation}'");
+            }
+            else
+            {
+                locationFilter.Append(@" like '%{pLocation}%'");
+            }
+
+            Outlook.Items appts = pAppointments;
+            appts.Restrict(locationFilter.ToString());
+            if (appts.Count > 0)
+            {
+                return appts;
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
