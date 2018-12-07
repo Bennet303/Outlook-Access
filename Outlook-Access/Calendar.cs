90056d139506570e1603c398bb403040b7502515
Bennet Hölscher <bennet.hoelscher@outlook.com>
2018-12-07 16:31:15 +0100

    Added AddAppointment() and AddReccuringAppointment() to the class Calendar

--- Outlook-Access/Calendar.cs
+++ Outlook-Access/Calendar.cs
@@ -24,7 +24,7 @@ namespace Outlook_Access
                 _OutlookNameSpace = _OutlookApplication.GetNamespace(pNamespace);
                 object username = pUsername;
                 object password = pPassword;
-                
+
                 if (username == null)
                 {
                     username = Missing.Value;
@@ -74,6 +74,9 @@ namespace Outlook_Access
             _CalendarItems = null;
         }
 
+        /*--------------------------------------------------------------------------------------------
+         * Reading
+         -------------------------------------------------------------------------------------------*/
 
         //Returning every appointment within a given interval 
         public List<Outlook.AppointmentItem> FindAppointmentsInRange(DateTime pStart, DateTime pEnd)
@@ -142,10 +145,139 @@ namespace Outlook_Access
             return FindAppointmentsByLocation(pLocations, pStart, pEnd, false);
         }
 
+        /*--------------------------------------------------------------------------------------------
+         * Writing
+         -------------------------------------------------------------------------------------------*/
+        public void AddAppointment(string pSubject, DateTime pStart, DateTime pEnd, string pLocation, bool pAllDay, string pBody, string pCategory)
+        {
+            InternalAddAppointment(pSubject, pStart, pEnd, pLocation, pAllDay, pBody, pCategory);
+        }
+
+        public void AddAppointment(string pSubject, DateTime pStart, double pLength, string pLocation, bool pAllDay, string pBody, string pCategory)
+        {
+            AddAppointment(pSubject, pStart, pStart.AddHours(pLength), pLocation, pAllDay, pBody, pCategory);
+        }
+
+        public void AddAppointment(string pSubject, DateTime pStart, DateTime pEnd, string pLocation, bool pAllDay, string pBody)
+        {
+            AddAppointment(pSubject, pStart, pEnd, pLocation, pAllDay, pBody, null);
+        }
+
+        public void AddAppointment(string pSubject, DateTime pStart, DateTime pEnd, string pLocation, bool pAllDay)
+        {
+            AddAppointment(pSubject, pStart, pEnd, pLocation, pAllDay, null);
+        }
+
+        public void AddAppointment(string pSubject, DateTime pStart, DateTime pEnd, string pLocation, string pBody)
+        {
+            AddAppointment(pSubject, pStart, pEnd, pLocation, false, pBody);
+        }
+
+        public void AddAppointment(string pSubject, DateTime pStart, double pLength, string pLocation)
+        {
+            AddAppointment(pSubject, pStart, pLength, pLocation, false, null, null);
+        }
+
+        public void AddAppointment(string pSubject, DateTime pStart, DateTime pEnd)
+        {
+            AddAppointment(pSubject, pStart, pEnd, null, false);
+        }
+
+        public void AddAppointment(string pSubject, DateTime pStart)
+        {
+            AddAppointment(pSubject, pStart, pStart.AddHours(1.5));
+        }
+
+        public Outlook.AppointmentItem AddReccuringAppointment(string pSubject, DateTime pStart, DateTime pEnd, string pLocation, bool pAllDay, string pBody, string pCategory)
+        {
+            return InternalAddAppointment(pSubject, pStart, pEnd, pLocation, pAllDay, pBody, pCategory);
+        }
+
+        public void EditRecurringItem(Outlook.AppointmentItem pRecurringAppointment, DateTime pPatternStart, DateTime pPatternEnd,
+            int pOccurrences, string pRecurrenceType, Enum pDaysOfWeek)
+        {
+            try
+            {
+                Outlook.RecurrencePattern recurrencePattern = pRecurringAppointment.GetRecurrencePattern();
+                recurrencePattern.PatternStartDate = pPatternStart.Date;
+
+                if (pPatternEnd != null)
+                {
+                    recurrencePattern.PatternEndDate = pPatternEnd;
+                }
+                else
+                {
+                    recurrencePattern.NoEndDate = true;
+                }
+
+                if (pOccurrences != 0)
+                {
+                    recurrencePattern.Occurrences = pOccurrences;
+                }
+
+                Outlook.OlRecurrenceType recurrenceType;
+                switch (pRecurrenceType.ToLower())
+                {
+                    case "daily": recurrenceType = Outlook.OlRecurrenceType.olRecursDaily; break;
+                    case "weekly": recurrenceType = Outlook.OlRecurrenceType.olRecursWeekly; break;
+                    case "monthly": recurrenceType = Outlook.OlRecurrenceType.olRecursMonthly; break;
+                    case "yearly": recurrenceType = Outlook.OlRecurrenceType.olRecursYearly; break;
+                    default: throw new ArgumentNullException("No viable RecurrenceType selected");
+                }
+                recurrencePattern.RecurrenceType = recurrenceType;
+
+                recurrencePattern.DayOfWeekMask = (Outlook.OlDaysOfWeek)pDaysOfWeek;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
         //--------------------------------------------------------------------------------------------------------------
         /* Methods for class internal use */
         //--------------------------------------------------------------------------------------------------------------
 
+
+        private Outlook.AppointmentItem InternalAddAppointment(string pSubject, DateTime pStart, DateTime pEnd, string pLocation, bool pAllDay, string pBody, string pCategory)
+        {
+            try
+            {
+                Outlook.AppointmentItem appointment = (Outlook.AppointmentItem)
+                    _OutlookApplication.CreateItem(Outlook.OlItemType.olAppointmentItem);
+                appointment.Subject = pSubject;
+                appointment.Start = pStart;
+                appointment.End = pEnd;
+                appointment.AllDayEvent = pAllDay;
+                if (pLocation != null)
+                {
+                    appointment.Location = pLocation;
+                }
+                if (pBody != null)
+                {
+                    appointment.Body = pBody;
+                }
+                if (pCategory != null)
+                {
+                    appointment.Categories = pCategory;
+                }
+
+                SaveAndDisplayAppointment(appointment);
+                return appointment;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e} Exception caught");
+                return null;
+            }
+        }
+
+        private static void SaveAndDisplayAppointment(Outlook.AppointmentItem pAppt)
+        {
+            pAppt.Save();
+            pAppt.Display(true);
+        }
+        
         private Outlook.Items RestrictByInterval(DateTime pStart, DateTime pEnd)
         {
             string filter = String.Format("[Start] >= '{0}' AND [End] < '{1}'",
