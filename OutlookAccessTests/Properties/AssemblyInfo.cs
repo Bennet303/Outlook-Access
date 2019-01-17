f482f20c45eacecba06180c83576e95db481cdf2
Bennet Hölscher <bennet.hoelscher@outlook.com>
2019-01-17 09:38:25 +0100

    Added the Unit Test Project "OutlookAccessTests"

---
+++ OutlookAccessTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OutlookAccessTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OutlookAccessTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(true)]
+
+[assembly: Guid("29cdd3f9-db01-4dc6-ab1c-af2a11a3d22f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
