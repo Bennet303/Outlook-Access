7c24137dad92e49460099456f7fa364088cb8579
Bennet Hölscher <bennet.hoelscher@outlook.com>
2019-01-17 09:36:38 +0100

    Minor changes to solution properties

--- Outlook-Access/Properties/AssemblyInfo.cs
+++ Outlook-Access/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
 // für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
 // COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("f58ca2cb-a4f6-4ff9-bf15-e6c3b11183b4")]
